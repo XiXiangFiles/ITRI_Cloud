b53036e059109dffdfaf9246dc56e1131db571d2
wongwong <luckboystudent@gmail.com>
2019-01-21 21:41:04 +0800

    v1.0.4

--- config/development_config.js
+++ config/development_config.js
@@ -6,5 +6,8 @@ module.exports = {
         user: process.env.DATABASE_USER,
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE
-    }
+    },
+	ip:{
+		local:process.env.HOST,
+	}
 }
